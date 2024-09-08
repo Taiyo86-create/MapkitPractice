1f7cf42f9a1e928403e63c827bf5b7faca988dea
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-09-08 22:25:23 +0900

    Initial Commit

---
+++ MapkitPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapkitPractice
+//
+//  Created by 水元太陽 on 2024/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapkitPractice/MapkitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapkitPracticeApp.swift
+//  MapkitPractice
+//
+//  Created by 水元太陽 on 2024/09/08.
+//
+
+import SwiftUI
+
+@main
+struct MapkitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
